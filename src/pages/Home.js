ccd6809c1ed0233cd9c6cf51635891f9cd665162
Dixit Parmar <Dparmar006@users.noreply.github.com>
2021-10-27 12:46:29 +0530

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Container>
+      <Typography variant="h2">Hello</Typography>
+    </Container>
+  );
+};
+
+export default Home;
