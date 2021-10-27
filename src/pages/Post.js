8e8d2d4671bdf2306681ef7421f7aa037d654a45
Dixit Parmar <Dparmar006@users.noreply.github.com>
2021-10-28 01:33:16 +0530

    Comments fetched

--- src/pages/Post.js
+++ src/pages/Post.js
@@ -1,5 +1,15 @@
 import { Favorite } from "@mui/icons-material";
-import { Container, Divider, ToggleButton, Typography } from "@mui/material";
+import {
+  Avatar,
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Divider,
+  Grid,
+  ToggleButton,
+  Typography,
+} from "@mui/material";
 import { red } from "@mui/material/colors";
 import { Box, typography } from "@mui/system";
 import React, { useEffect, useState } from "react";
@@ -10,6 +20,19 @@ import BlogAppBar from "../components/BlogAppBar";
 const Post = () => {
   const { id } = useParams();
   const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const retriveData = async () => {
+      try {
+        const { data } = await api.get(`/comments`);
+        setComments(data.filter((comment) => comment.postId === Number(id)));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    retriveData();
+  });
   useEffect(() => {
     const retriveData = async () => {
       try {
@@ -46,6 +69,26 @@ const Post = () => {
         </Box>
         <Divider sx={{ marginY: "1rem" }} />
         <Typography>{post.body}</Typography>
+        <Divider sx={{ marginY: "1rem" }} />
+      </Container>
+      <Container>
+        <Typography color="GrayText" variant="h4">
+          Comments
+          <Grid container spacing="18">
+            {comments.map((comment) => {
+              return (
+                <Grid item xl={3} lg={4} md={6} xs={12}>
+                  <Card sx={{ padding: "1rem" }}>
+                    <Box>
+                      <Avatar sx={{ bgColor: red[500] }}>R</Avatar>
+                    </Box>
+                    <Typography>{comment.body}</Typography>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
+        </Typography>
       </Container>
     </React.Fragment>
   );
